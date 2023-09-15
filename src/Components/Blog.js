b50885c40152f665b4a21d06ea801c36cd4f8b15
Praveen Saw <praveen.kumar.paytm2019@gmail.com>
2023-09-15 11:39:37 +0530

    Apply Hooks

--- src/Components/Blog.js
+++ src/Components/Blog.js
@@ -1,6 +1,9 @@
 //Blogging App using Hooks
+import  { useState } from 'react';
 export default function Blog(){
     
+    const[title,setTitle] = useState("");
+    const[content, setContent] = useState("");
     //Passing the synthetic event as argument to stop refreshing the page on submit
     function handleSubmit(e){
         e.preventDefault();
@@ -20,13 +23,15 @@ export default function Blog(){
                 {/* Row component to create a row for first input field */}
                 <Row label="Title">
                         <input className="input"
-                                placeholder="Enter the Title of the Blog here.."/>
+                                placeholder="Enter the Title of the Blog here.."
+                                value={title} onChange={(e) => setTitle(e.target.value)}/>
                 </Row >
 
                 {/* Row component to create a row for Text area field */}
                 <Row label="Content">
                         <textarea className="input content"
-                                placeholder="Content of the Blog goes here.."/>
+                                placeholder="Content of the Blog goes here.."
+                                value={content} onChange={(e)=> setContent(e.target.value)}/>
                 </Row >
 
                 {/* Button to submit the blog */}            
@@ -39,6 +44,8 @@ export default function Blog(){
 
         {/* Section where submitted blogs will be displayed */}
         <h2> Blogs </h2>
+        <h1>{title}</h1>
+        <p>{content}</p>
         
         </>
         )
